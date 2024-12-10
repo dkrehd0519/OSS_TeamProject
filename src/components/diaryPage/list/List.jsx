bb5a8bace1863d87da483c4bbeacdd8771c2abba
JongHyun <jonghyun@JongHyuns-MacBook-Pro.local>
2024-12-10 20:36:52 +0900

    bug fix

--- src/components/diaryPage/list/List.jsx
+++ src/components/diaryPage/list/List.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "../../common/Header";
 import Diaries from "./Diaries";
-import TextField from "./TextField";
 
 function List() {
   return (
     <div>
       <Header />
       <Diaries />
-      <TextField />
     </div>
   );
 }
